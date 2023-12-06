479661e8b80e62dc5bfd4d1adf9fa465f8f52eb9
Tech Advice <60535290+Cdndelivr@users.noreply.github.com>
2023-12-06 14:00:30 +0530

    Update serveJsFile.js

--- functions/serveJsFile.js
+++ functions/serveJsFile.js
@@ -3,33 +3,39 @@ const fetch = require('node-fetch');
 exports.handler = async (event, context) => {
   const { domain } = JSON.parse(event.body);
 
-  // Verify the domain using the verifyDomain function
-  const response = await fetch(`${process.env.URL}/.netlify/functions/verifyDomain`, {
-    method: 'POST',
-    body: JSON.stringify({ domain }),
-  });
+  try {
+    const response = await fetch(`${process.env.URL}/.netlify/functions/verifyDomain`, {
+      method: 'POST',
+      body: JSON.stringify({ domain }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
 
-  if (response.ok) {
-    // Domain is authorized, serve the JavaScript file
+    if (!response.ok) {
+      throw new Error('Unauthorized domain.');
+    }
+
+    const jsCode = 'console.log("Your JavaScript code");';
+
     return {
       statusCode: 200,
       headers: {
-        'Access-Control-Allow-Origin': 'https://www.codeadvice.xyz', // Replace with your actual domain
+        'Access-Control-Allow-Origin': '*', // Adjust this based on your requirements
         'Access-Control-Allow-Headers': 'Content-Type',
         'Access-Control-Allow-Methods': 'POST',
       },
-      body: 'console.log("Your JavaScript code");',
+      body: jsCode,
     };
-  } else {
-    // Domain is not authorized, return an error or placeholder script
+  } catch (error) {
     return {
       statusCode: 403,
       headers: {
-        'Access-Control-Allow-Origin': 'https://www.codeadvice.xyz', // Replace with your actual domain
+        'Access-Control-Allow-Origin': '*', // Adjust this based on your requirements
         'Access-Control-Allow-Headers': 'Content-Type',
         'Access-Control-Allow-Methods': 'POST',
       },
-      body: 'console.error("Unauthorized domain.");',
+      body: `console.error("${error.message}");`,
     };
   }
 };
