97894dbf055ac6d0380e55e2bfc03676569726bc
Tech Advice <60535290+Cdndelivr@users.noreply.github.com>
2023-12-06 14:43:53 +0530

    Update verifyDomain.js

--- functions/verifyDomain.js
+++ functions/verifyDomain.js
@@ -1,17 +1,24 @@
-const allowedDomains = require('./allowedDomains.json');
+const allowedDomains = [
+  "safelink-pro.netlify.app",
+  // Add more domains as needed
+];
 
 exports.handler = async (event, context) => {
-  const { domain } = JSON.parse(event.body);
+  try {
+    const { domain } = JSON.parse(event.body);
 
-  if (allowedDomains.includes(domain)) {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: 'Domain is authorized.' }),
-    };
-  } else {
+    if (allowedDomains.includes(domain)) {
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ message: 'Domain is authorized.' }),
+      };
+    } else {
+      throw new Error('Unauthorized domain.');
+    }
+  } catch (error) {
     return {
       statusCode: 403,
-      body: JSON.stringify({ message: 'Unauthorized domain.' }),
+      body: JSON.stringify({ message: error.message }),
     };
   }
 };
