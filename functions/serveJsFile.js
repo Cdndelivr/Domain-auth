875b4ffb1918da40d9c5db1fdbabd862a9da47d0
Tech Advice <60535290+Cdndelivr@users.noreply.github.com>
2023-12-06 17:59:34 +0530

    Update serveJsFile.js

--- functions/serveJsFile.js
+++ functions/serveJsFile.js
@@ -1,47 +1,35 @@
 const fetch = require('node-fetch');
 
 exports.handler = async (event, context) => {
-  try {
-    // Ensure that event.body is not empty
-    if (!event.body) {
-      throw new Error('Request body is empty.');
-    }
+  const { domain } = JSON.parse(event.body);
 
-    const { domain } = JSON.parse(event.body);
+  // Verify the domain using the verifyDomain function
+  const response = await fetch(`${process.env.URL}/.netlify/functions/verifyDomain`, {
+    method: 'POST',
+    body: JSON.stringify({ domain }),
+  });
 
-    // Verify the domain using the verifyDomain function
-    const response = await fetch(`${process.env.URL}/.netlify/functions/verifyDomain`, {
-      method: 'POST',
-      body: JSON.stringify({ domain }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-
-    if (!response.ok) {
-      throw new Error('Unauthorized domain.');
-    }
-
-    const jsCode = 'console.log("Your JavaScript code");';
-
+  if (response.ok) {
+    // Domain is authorized, serve the JavaScript file
     return {
       statusCode: 200,
       headers: {
-        'Access-Control-Allow-Origin': '*', // Adjust this based on your requirements
+        'Access-Control-Allow-Origin': '*', // Allow requests from any origin
         'Access-Control-Allow-Headers': 'Content-Type',
         'Access-Control-Allow-Methods': 'POST',
       },
-      body: jsCode,
+      body: 'console.log("Your JavaScript code");',
     };
-  } catch (error) {
+  } else {
+    // Domain is not authorized, return an error or placeholder script
     return {
       statusCode: 403,
       headers: {
-        'Access-Control-Allow-Origin': '*', // Adjust this based on your requirements
+        'Access-Control-Allow-Origin': '*', // Allow requests from any origin
         'Access-Control-Allow-Headers': 'Content-Type',
         'Access-Control-Allow-Methods': 'POST',
       },
-      body: `console.error("${error.message}");`,
+      body: 'console.error("Unauthorized domain.");',
     };
   }
 };
