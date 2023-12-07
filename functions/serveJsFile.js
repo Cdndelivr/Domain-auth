0c52ab57537ed984495f4923b66e8d5dfd75c4ee
Tech Advice <60535290+Cdndelivr@users.noreply.github.com>
2023-12-07 14:22:50 +0530

    Update serveJsFile.js

--- functions/serveJsFile.js
+++ functions/serveJsFile.js
@@ -2,55 +2,43 @@ const fetch = require('node-fetch');
 
 exports.handler = async (event, context) => {
   const { domain } = JSON.parse(event.body);
+
   try {
-    const { domain } = JSON.parse(event.body);
-
-  // Verify the domain using the verifyDomain function
-  const response = await fetch(`${process.env.URL}/.netlify/functions/verifyDomain`, {
-    method: 'POST',
-    body: JSON.stringify({ domain }),
-  });
-
-  if (response.ok) {
-    // Domain is authorized, serve the JavaScript file
-    return {
-      statusCode: 200,
+    const response = await fetch(`${process.env.URL}/.netlify/functions/verifyDomain`, {
+      method: 'POST',
+      body: JSON.stringify({ domain }),
       headers: {
-         res.setHeader("Access-Control-Allow-Origin", "*"); // Allow requests from any origin
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*', // Allow requests from any origin, adjust as needed
         'Access-Control-Allow-Headers': 'Content-Type',
         'Access-Control-Allow-Methods': 'POST',
       },
-      body: 'console.log("Your JavaScript code");',
-    };
-  } else {
-    // Domain is not authorized, return an error or placeholder script
-    // Allow requests from your local development environment
-    const allowedOrigins = ['http://127.0.0.1:3000'];
-    const origin = event.headers.origin || '';
-    if (allowedOrigins.includes(origin)) {
-      return {
-        statusCode: 200,
-        headers: {
-          'Access-Control-Allow-Origin': origin,
-          'Access-Control-Allow-Headers': 'Content-Type',
-          'Access-Control-Allow-Methods': 'POST',
-        },
-        body: 'console.log("Your JavaScript code");',
-      };
-    } else {
-      throw new Error('Unauthorized origin.');
+    });
+
+    if (!response.ok) {
+      throw new Error('Unauthorized domain.');
     }
+
+    const jsCode = 'console.log("Your JavaScript code");';
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*', // Allow requests from any origin, adjust as needed
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'POST',
+      },
+      body: jsCode,
+    };
   } catch (error) {
     return {
       statusCode: 403,
       headers: {
-        'Access-Control-Allow-Origin': '*', // Allow requests from any origin
-        'Access-Control-Allow-Origin': '*', // Adjust this based on your requirements
+        'Access-Control-Allow-Origin': '*', // Allow requests from any origin, adjust as needed
         'Access-Control-Allow-Headers': 'Content-Type',
         'Access-Control-Allow-Methods': 'POST',
       },
-      body: 'console.error("Unauthorized domain.");',
-      body: `console.error("${error.message || 'Request body is empty.'}");`,
+      body: `console.error("${error.message}");`,
     };
   }
 };
