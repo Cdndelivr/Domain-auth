2e6e7bfbce5710d5f39e0c8e1633cccb635726d3
Tech Advice <60535290+Cdndelivr@users.noreply.github.com>
2023-12-10 20:18:18 +0530

    Update verifyDomain.js

--- functions/verifyDomain.js
+++ functions/verifyDomain.js
@@ -1,20 +1,35 @@
-const sharedSecret = "QWERTY"; // Replace with your actual secret
-
+// functions/verifyDomain.js
 exports.handler = async (event, context) => {
-  const { domain, secret } = JSON.parse(event.body);
+  const { domain } = JSON.parse(event.body);
 
-  if (secret !== sharedSecret) {
+  const headers = {
+    'Access-Control-Allow-Origin': '*',  // Adjust this based on your requirements
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Methods': 'POST',
+  };
+
+  if (event.httpMethod === 'OPTIONS') {
+    // Handle CORS preflight request
     return {
-      statusCode: 403,
-      body: JSON.stringify({ message: 'Unauthorized request.' }),
+      statusCode: 200,
+      headers,
+      body: '',
     };
   }
 
-  // Your existing domain verification logic
-  // ...
+  // Your existing logic for domain verification goes here
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Domain is authorized.' }),
-  };
+  if (allowedDomains.includes(domain)) {
+    return {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify({ message: 'Domain is authorized.' }),
+    };
+  } else {
+    return {
+      statusCode: 403,
+      headers,
+      body: JSON.stringify({ message: 'Unauthorized domain.' }),
+    };
+  }
 };
