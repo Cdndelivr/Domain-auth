ad40a8c46344887d90b652566f8a59b9bc41e55d
Tech Advice <60535290+Cdndelivr@users.noreply.github.com>
2023-12-14 09:01:07 +0530

    Update serveJsFile.js

--- functions/serveJsFile.js
+++ functions/serveJsFile.js
@@ -1,27 +1,40 @@
-// functions/serveJsFile.js
-
 const fs = require('fs');
+const jwt = require('jsonwebtoken');
 
 exports.handler = async function (event, context) {
+  const secretKey = '123456789'; // Replace with a secure secret key
   const allowedDomains = ['https://www.codeadvice.xyz', 'https://subdomain.example.com'];
-  const origin = event.headers.origin;
 
-  if (allowedDomains.includes(origin)) {
-    // Read and serve the JavaScript file
-    const jsCode = fs.readFileSync('/src/V3-Latest.js', 'utf-8');
+  // Get the token from the request headers
+  const token = event.headers.authorization;
 
+  try {
+    // Verify the token
+    const decoded = jwt.verify(token, secretKey);
+
+    // Check if the domain is authorized
+    const origin = decoded.origin;
+    if (allowedDomains.includes(origin)) {
+      // Read and serve the JavaScript file
+      const jsCode = fs.readFileSync('/src/V3-Latest.js', 'utf-8');
+
+      return {
+        statusCode: 200,
+        body: jsCode,
+        headers: {
+          'Content-Type': 'application/javascript',
+        },
+      };
+    } else {
+      return {
+        statusCode: 403,
+        body: JSON.stringify({ error: 'Unauthorized domain' }),
+      };
+    }
+  } catch (error) {
     return {
-      statusCode: 200,
-      body: jsCode,
-      headers: {
-        'Content-Type': 'application/javascript',
-        'Access-Control-Allow-Origin': origin,
-      },
-    };
-  } else {
-    return {
-      statusCode: 403,
-      body: JSON.stringify({ error: 'Unauthorized domain' }),
+      statusCode: 401,
+      body: JSON.stringify({ error: 'Unauthorized' }),
     };
   }
 };
