bf12df93a39b551df252680e5c3b19ee3257f45a
Tech Advice <60535290+Cdndelivr@users.noreply.github.com>
2023-12-17 12:40:19 +0530

    Update serveJsFile.js

--- functions/serveJsFile.js
+++ functions/serveJsFile.js
@@ -1,14 +1,32 @@
-exports.handler = async (event, context) => {
-  // Fetch and serve your JavaScript code
-  const jsCode = 'console.log("Hello from server-side!");';
+// functions/serveJsFile.js
 
+exports.handler = async (event, context) => {
+  const { headers } = event;
+
+  // Check if the 'Origin' header is present
+  if ('origin' in headers) {
+    const origin = headers['origin'];
+
+    // Replace 'https://www.codeadvice.xyz' with the actual authorized domain
+    if (origin === 'https://www.codeadvice.xyz') {
+      // Fetch and serve your JavaScript code
+      const jsCode = 'console.log("Hello from server-side!");';
+
+      return {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': origin,
+          'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+          'Access-Control-Allow-Headers': 'Content-Type',
+        },
+        body: jsCode,
+      };
+    }
+  }
+
+  // Unauthorized access
   return {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': 'https://www.codeadvice.xyz',
-      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-      'Access-Control-Allow-Headers': 'Content-Type',
-    },
-    body: jsCode,
+    statusCode: 403,
+    body: JSON.stringify({ error: 'Unauthorized' }),
   };
 };
