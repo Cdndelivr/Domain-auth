e78cd080c8f022e600e89e9a4d34a514c0df7063
Tech Advice <60535290+Cdndelivr@users.noreply.github.com>
2023-12-17 12:34:53 +0530

    Update serveJsFile.js

--- functions/serveJsFile.js
+++ functions/serveJsFile.js
@@ -1,32 +1,14 @@
-// functions/serveJsFile.js
+exports.handler = async (event, context) => {
+  // Fetch and serve your JavaScript code
+  const jsCode = 'console.log("Hello from server-side!");';
 
-const fs = require('fs');
-const jwt = require('jsonwebtoken');
-
-exports.handler = async function (event, context) {
-  const secretKey = 'QWERTY123';
-
-  const token = event.headers.authorization;
-
-  try {
-    const decoded = jwt.verify(token, secretKey);
-
-    const jsCode = fs.readFileSync('./src/V3-Latest.js', 'utf-8');
-
-    return {
-      statusCode: 200,
-      body: jsCode,
-      headers: {
-        'Content-Type': 'application/javascript',
-        'Access-Control-Allow-Origin': '*', // Allow requests from any origin for testing purposes
-        'Access-Control-Allow-Methods': 'GET',
-        'Access-Control-Allow-Headers': 'Authorization',
-      },
-    };
-  } catch (error) {
-    return {
-      statusCode: 401,
-      body: JSON.stringify({ error: 'Unauthorized' }),
-    };
-  }
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': 'https://www.codeadvice.xyz',
+      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type',
+    },
+    body: jsCode,
+  };
 };
