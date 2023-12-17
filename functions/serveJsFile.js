315e076397de878a2d0aec2dee568698199993e6
Tech Advice <60535290+Cdndelivr@users.noreply.github.com>
2023-12-17 16:04:34 +0530

    Update serveJsFile.js

--- functions/serveJsFile.js
+++ functions/serveJsFile.js
@@ -32,7 +32,7 @@ exports.handler = async (event, context) => {
   // Unauthorized access
   return {
     statusCode: 403,
-    body: JSON.stringify('Hyy... Code Is Not Found Here'),
+    body: JSON.stringify('Code Is Not Found Here.. 😜'),
   };
 };
 
