6a3b7dcbd39b27441669671354378c1a39c6a53d
Tech Advice <60535290+Cdndelivr@users.noreply.github.com>
2023-12-17 22:50:59 +0530

    Update serveJsFile.js

--- functions/serveJsFile.js
+++ functions/serveJsFile.js
@@ -14,6 +14,9 @@ exports.handler = async (event, context) => {
 
     // Check if the origin is in the list of authorized domains
     if (authorizedDomains.includes(origin)) {
+      // Fetch and serve your JavaScript code
+      const jsCode = 'console.log("Hello from server-side! - Devil");';
+
       return {
         statusCode: 200,
         headers: {
@@ -21,7 +24,7 @@ exports.handler = async (event, context) => {
           'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
           'Access-Control-Allow-Headers': 'Content-Type',
         },
-        body: '',
+        body: jsCode,
       };
     }
   }
@@ -29,46 +32,6 @@ exports.handler = async (event, context) => {
   // Unauthorized access
   return {
     statusCode: 403,
-    body: JSON.stringify('Unauthorized'),
+    body: JSON.stringify('Code Is Not Found Here.. 😜'),
   };
 };
-
-
-
-// exports.handler = async (event, context) => {
-//   const { headers } = event;
-
-//   // List of authorized domains
-//   const authorizedDomains = [
-//     'https://www.codeadvice.xyz',
-//     // 'https://demo-techadvicev4.blogspot.com',
-//     // Add more authorized domains as needed
-//   ];
-
-//   // Check if the 'Origin' header is present
-//   if ('origin' in headers) {
-//     const origin = headers['origin'];
-
-//     // Check if the origin is in the list of authorized domains
-//     if (authorizedDomains.includes(origin)) {
-//       // Fetch and serve your JavaScript code
-//       const jsCode = 'console.log("Hello from server-side! - Devil");';
-
-//       return {
-//         statusCode: 200,
-//         headers: {
-//           'Access-Control-Allow-Origin': origin,
-//           'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-//           'Access-Control-Allow-Headers': 'Content-Type',
-//         },
-//         body: jsCode,
-//       };
-//     }
-//   }
-
-//   // Unauthorized access
-//   return {
-//     statusCode: 403,
-//     body: JSON.stringify('Code Is Not Found Here.. 😜'),
-//   };
-// };
