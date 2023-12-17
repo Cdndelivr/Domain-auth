65180dfc29c3050b42828256767a26f9f4d7bfbb
Tech Advice <60535290+Cdndelivr@users.noreply.github.com>
2023-12-17 14:38:24 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: './src/V3-Latest.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'functions'),
   },
   optimization: {
     minimize: true,
