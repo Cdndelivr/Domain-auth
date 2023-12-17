dbf9f803ee7f09ce79ed2ebcf5d5c1b8901f9485
Tech Advice <60535290+Cdndelivr@users.noreply.github.com>
2023-12-17 15:44:59 +0530

    Update serveJsFile.js

--- functions/serveJsFile.js
+++ functions/serveJsFile.js
@@ -34,7 +34,7 @@ exports.handler = async (event, context) => {
 
   // Unauthorized access
   return {
-    body: JSON.stringify("Hello, Are You Try To Copy My Code... Don't Worry you can do that"),
+        body: "Are You Trying To Copy My Code? Don't Worry, i can't do that!",
   };
 };
 
