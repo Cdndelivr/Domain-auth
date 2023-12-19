05c109b9b7c695deec81384438a20e2e4c03f4d1
Tech Advice <60535290+Cdndelivr@users.noreply.github.com>
2023-12-19 18:07:27 +0530

    Delete src/V3-Latest.js

--- src/V3-Latest.js
+++
@@ -1,9 +0,0 @@
-const feedUrl = "/feeds/posts/default?alt=json&max-results=1000";
-const links = [];
-
-fetch(feedUrl)
-  .then(response => response.json())
-  .then(data => {
-    links.push(...(data.feed.entry || []).map(entry => entry.link.find(link => link.rel === "alternate").href));
-  })
-  .catch(error => console.error("Error fetching Posts:"));
