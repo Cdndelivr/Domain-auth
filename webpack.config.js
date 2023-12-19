cf160945c6e8e0e07b8d0e146064576b2db15a37
Tech Advice <60535290+Cdndelivr@users.noreply.github.com>
2023-12-19 20:22:54 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
-const path = require('path');
+// const path = require('path');
 
-module.exports = {
-  entry: './src/V3-Latest.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'functions'),
-  },
-  optimization: {
-    minimize: true,
-  },
-};
+// module.exports = {
+//   entry: './src/V3-Latest.js',
+//   output: {
+//     filename: 'bundle.js',
+//     path: path.resolve(__dirname, 'functions'),
+//   },
+//   optimization: {
+//     minimize: true,
+//   },
+// };
