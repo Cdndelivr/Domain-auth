c7d2583c54f213b0d599e8b577fe4270f2b54998
Tech Advice <60535290+Cdndelivr@users.noreply.github.com>
2023-12-19 20:48:54 +0530

    Update serveJsFile.js

--- functions/serveJsFile.js
+++ functions/serveJsFile.js
@@ -7,36 +7,34 @@ exports.handler = async (event, context) => {
     // 'https://demo-techadvicev4.blogspot.com',
   ];
 
-  // Check if the 'Origin' header is present
   if ('origin' in headers) {
     const origin = headers['origin'];
 
     // Check if the origin is in the list of authorized domains
-if (authorizedDomains.includes(origin)) {
-  // Fetch and serve your JavaScript code
-  const jsCode = `
-    console.log("Successfully Connected");
-    fetch(feedUrl)
-      .then(response => response.json())
-      .then(data => {
-        links.push(...(data.feed.entry || []).map(entry => entry.link.find(link => link.rel === "alternate").href));
-      })
-      .catch(error => console.error("Error fetching Posts:", error));
-  `;
-  
-  return {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': origin,
-      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-      'Access-Control-Allow-Headers': 'Content-Type',
-    },
-    body: jsCode,
-  };
-} else {
-  // Unauthorized access
-  console.log("Unauthorized");
-  
+    if (authorizedDomains.includes(origin)) {
+      // Fetch and serve your JavaScript code
+     const jsCode = `
+  console.log("Successfully Connected With Server");
+  fetch(feedUrl)
+    .then(response => response.json())
+    .then(data => {
+      links.push(...(data.feed.entry || []).map(entry => entry.link.find(link => link.rel === "alternate").href));
+    })
+    .catch(error => console.error("Error fetching Posts:", error));
+`;
+
+      return {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': origin,
+          'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+          'Access-Control-Allow-Headers': 'Content-Type',
+        },
+        body: jsCode,
+      };
+    }
+  }
+
   return {
     statusCode: 403,
     body: JSON.stringify('Code Is Not Found Here.. 😜'),
