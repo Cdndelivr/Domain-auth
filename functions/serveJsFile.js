abf6ce7e2a0bfa2b98705756fb755780fac6a0a0
Tech Advice <60535290+Cdndelivr@users.noreply.github.com>
2023-12-22 09:02:42 +0530

    Update serveJsFile.js

--- functions/serveJsFile.js
+++ functions/serveJsFile.js
@@ -1,56 +1,33 @@
 const fs = require('fs');
 const path = require('path');
-const authorizedDomains = require('./domains');
 
 exports.handler = async (event, context) => {
-  const { headers } = event;
+  try {
+    const jsFilePath = path.join(__dirname, 'src/V3-Latest.js');
+    const jsCode = await fs.promises.readFile(jsFilePath, 'utf8');
 
-  if ('origin' in headers) {
-    const origin = headers['origin'];
-
-    if (authorizedDomains.includes(origin)) {
-      const jsFilePath = path.join(__dirname, 'src/V3-Latest.js');
-
-      try {
-        // Read the content of the JavaScript file asynchronously
-        const jsCode = await fs.promises.readFile(jsFilePath, 'utf8');
-
-        return {
-          statusCode: 200,
-          headers: {
-            'Access-Control-Allow-Origin': origin,
-            'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-            'Access-Control-Allow-Headers': 'Content-Type',
-          },
-          body: jsCode,
-        };
-      } catch (error) {
-        console.error('Error reading JavaScript file:', error);
-        return {
-          statusCode: 500,
-          headers: {
-            'Access-Control-Allow-Origin': origin,
-            'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-            'Access-Control-Allow-Headers': 'Content-Type',
-          },
-          body: JSON.stringify({
-            error: 'Internal Server Error',
-            details: error.message, // Include the error message for debugging
-          }),
-        };
-      }
-    }
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type',
+      },
+      body: jsCode,
+    };
+  } catch (error) {
+    console.error('Error reading JavaScript file:', error);
+    return {
+      statusCode: 500,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type',
+      },
+      body: JSON.stringify({
+        error: 'Internal Server Error',
+        details: error.message,
+      }),
+    };
   }
-
-  console.warn('Unauthorized access attempt. Origin not found in authorized domains.');
-
-  return {
-    statusCode: 403,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-      'Access-Control-Allow-Headers': 'Content-Type',
-    },
-    body: JSON.stringify('Unauthorized website access. 😜'),
-  };
 };
