c6a7fdc0b788179a348d0bba60f12e39f1be2e88
Tech Advice <60535290+Cdndelivr@users.noreply.github.com>
2023-12-22 09:05:40 +0530

    Update serveJsFile.js

--- functions/serveJsFile.js
+++ functions/serveJsFile.js
@@ -1,33 +1,36 @@
-const fs = require('fs');
-const path = require('path');
+const authorizedDomains = require('./domains');
 
 exports.handler = async (event, context) => {
-  try {
-    const jsFilePath = path.join(__dirname, 'src/V3-Latest.js');
-    const jsCode = await fs.promises.readFile(jsFilePath, 'utf8');
+  const { headers } = event;
 
-    return {
-      statusCode: 200,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type',
-      },
-      body: jsCode,
-    };
-  } catch (error) {
-    console.error('Error reading JavaScript file:', error);
-    return {
-      statusCode: 500,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type',
-      },
-      body: JSON.stringify({
-        error: 'Internal Server Error',
-        details: error.message,
-      }),
-    };
+  if ('origin' in headers) {
+    const origin = headers['origin'];
+    
+    if (authorizedDomains.includes(origin)) {
+      const jsCode = `
+        console.log("Successfully Connected With Server");
+        fetch(feedUrl)
+          .then(response => response.json())
+          .then(data => {
+            links.push(...(data.feed.entry || []).map(entry => entry.link.find(link => link.rel === "alternate").href));
+          })
+          .catch(error => console.error("Error fetching Posts:", error));
+      `;
+
+      return {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': origin,
+          'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+          'Access-Control-Allow-Headers': 'Content-Type',
+        },
+        body: jsCode,
+      };
+    }
   }
+
+  return {
+    statusCode: 403,
+    body: JSON.stringify('Code Is Not Found Here.. 😜'),
+  };
 };
