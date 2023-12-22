a505cc4745cec4c0136fbd6e6b8b58f3cfce3103
Tech Advice <60535290+Cdndelivr@users.noreply.github.com>
2023-12-22 08:29:06 +0530

    Update serveJsFile.js

--- functions/serveJsFile.js
+++ functions/serveJsFile.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const path = require('path');
 const authorizedDomains = require('./domains');
 
 exports.handler = async (event, context) => {
@@ -5,27 +7,31 @@ exports.handler = async (event, context) => {
 
   if ('origin' in headers) {
     const origin = headers['origin'];
-    
-    if (authorizedDomains.includes(origin)) {
-      const jsCode = `
-        console.log("Successfully Connected With Server");
-        fetch(feedUrl)
-          .then(response => response.json())
-          .then(data => {
-            links.push(...(data.feed.entry || []).map(entry => entry.link.find(link => link.rel === "alternate").href));
-          })
-          .catch(error => console.error("Error fetching Posts:", error));
-      `;
 
-      return {
-        statusCode: 200,
-        headers: {
-          'Access-Control-Allow-Origin': origin,
-          'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-          'Access-Control-Allow-Headers': 'Content-Type',
-        },
-        body: jsCode,
-      };
+    if (authorizedDomains.includes(origin)) {
+      
+       const jsFilePath = path.join(__dirname, 'functions/bundle.js');
+
+      try {
+        // Read the content of the JavaScript file
+        const jsCode = fs.readFileSync(jsFilePath, 'utf8');
+
+        return {
+          statusCode: 200,
+          headers: {
+            'Access-Control-Allow-Origin': origin,
+            'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type',
+          },
+          body: jsCode,
+        };
+      } catch (error) {
+        console.error('Error reading JavaScript file:', error);
+        return {
+          statusCode: 500,
+          body: JSON.stringify('Internal Server Error'),
+        };
+      }
     }
   }
 
@@ -34,3 +40,50 @@ exports.handler = async (event, context) => {
     body: JSON.stringify('Code Is Not Found Here.. 😜'),
   };
 };
+
+
+
+
+
+
+
+
+
+
+
+// const authorizedDomains = require('./domains');
+
+// exports.handler = async (event, context) => {
+//   const { headers } = event;
+
+//   if ('origin' in headers) {
+//     const origin = headers['origin'];
+    
+//     if (authorizedDomains.includes(origin)) {
+//       const jsCode = `
+//         console.log("Successfully Connected With Server");
+//         fetch(feedUrl)
+//           .then(response => response.json())
+//           .then(data => {
+//             links.push(...(data.feed.entry || []).map(entry => entry.link.find(link => link.rel === "alternate").href));
+//           })
+//           .catch(error => console.error("Error fetching Posts:", error));
+//       `;
+
+//       return {
+//         statusCode: 200,
+//         headers: {
+//           'Access-Control-Allow-Origin': origin,
+//           'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+//           'Access-Control-Allow-Headers': 'Content-Type',
+//         },
+//         body: jsCode,
+//       };
+//     }
+//   }
+
+//   return {
+//     statusCode: 403,
+//     body: JSON.stringify('Code Is Not Found Here.. 😜'),
+//   };
+// };
