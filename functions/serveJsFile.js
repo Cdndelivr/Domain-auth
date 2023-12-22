f243bbea34b688b9d1766f490e3050815d066ed4
Tech Advice <60535290+Cdndelivr@users.noreply.github.com>
2023-12-22 10:32:32 +0530

    Update serveJsFile.js

--- functions/serveJsFile.js
+++ functions/serveJsFile.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const path = require('path');
 const authorizedDomains = require('./domains');
 
 exports.handler = async (event, context) => {
@@ -5,32 +7,50 @@ exports.handler = async (event, context) => {
 
   if ('origin' in headers) {
     const origin = headers['origin'];
-    
-    if (authorizedDomains.includes(origin)) {
-      const jsCode = `
-        console.log("Successfully Connected With Server");
-        fetch(feedUrl)
-          .then(response => response.json())
-          .then(data => {
-            links.push(...(data.feed.entry || []).map(entry => entry.link.find(link => link.rel === "alternate").href));
-          })
-          .catch(error => console.error("Error fetching Posts:", error));
-      `;
 
-      return {
-        statusCode: 200,
-        headers: {
-          'Access-Control-Allow-Origin': origin,
-          'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-          'Access-Control-Allow-Headers': 'Content-Type',
-        },
-        body: jsCode,
-      };
+    if (authorizedDomains.includes(origin)) {
+      const jsFilePath = path.join(__dirname, 'src/V3-Latest.js');
+
+      try {
+        // Read the content of the JavaScript file asynchronously
+        const jsCode = await fs.promises.readFile(jsFilePath, 'utf8');
+
+        return {
+          statusCode: 200,
+          headers: {
+            'Access-Control-Allow-Origin': origin,
+            'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type',
+          },
+          body: jsCode,
+        };
+      } catch (error) {
+        console.error('Error reading JavaScript file:', error);
+        return {
+          statusCode: 500,
+          headers: {
+            'Access-Control-Allow-Origin': origin,
+            'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type',
+          },
+          body: JSON.stringify({
+            error: 'Internal Server Error',
+            details: error.message,
+          }),
+        };
+      }
     }
   }
 
+  console.warn('Unauthorized access attempt. Origin not found in authorized domains.');
+
   return {
     statusCode: 403,
-    body: JSON.stringify('Code Is Not Found Here.. 😜'),
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type',
+    },
+    body: JSON.stringify('Unauthorized website access. 😜'),
   };
 };
