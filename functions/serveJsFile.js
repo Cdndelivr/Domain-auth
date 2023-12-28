67f31d0ed4bfed56c8b78722baf444068de20dc9
Tech Advice <60535290+Cdndelivr@users.noreply.github.com>
2023-12-28 08:23:45 +0530

    Update serveJsFile.js

--- functions/serveJsFile.js
+++ functions/serveJsFile.js
@@ -1,117 +1,117 @@
-const fs = require('fs');
-const path = require('path');
-const authorizedDomains = require('./domains');
-
-exports.handler = async (event, context) => {
-  const { headers } = event;
-
-  if ('origin' in headers) {
-    const origin = headers['origin'];
-
-    // Check if the requesting domain is authorized
-    if (authorizedDomains.includes(origin)) {
-      // Construct the file path
-      const jsFilePath = path.resolve(__dirname, 'src/V3-latest.js');
-
-      // Check if the file exists
-      if (fs.existsSync(jsFilePath)) {
-        console.log('File exists:', jsFilePath);
-
-        // Read the content of the JavaScript file
-        try {
-          const jsCode = fs.readFileSync(jsFilePath, 'utf8');
-
-          console.log('File read successfully');
-
-          return {
-            statusCode: 200,
-            headers: {
-              'Access-Control-Allow-Origin': origin,
-              'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-              'Access-Control-Allow-Headers': 'Content-Type',
-            },
-            body: jsCode,
-          };
-        } catch (error) {
-          console.error('Error reading file:', error);
-
-          return {
-            statusCode: 500,
-            headers: {
-              'Access-Control-Allow-Origin': origin,
-              'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-              'Access-Control-Allow-Headers': 'Content-Type',
-            },
-            body: JSON.stringify({
-              error: 'Internal Server Error',
-              details: error.message,
-            }),
-          };
-        }
-      } else {
-        console.error('File does not exist:', jsFilePath);
-
-        return {
-          statusCode: 404,
-          headers: {
-            'Access-Control-Allow-Origin': origin,
-            'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-            'Access-Control-Allow-Headers': 'Content-Type',
-          },
-          body: JSON.stringify({
-            error: 'File Not Found',
-          }),
-        };
-      }
-    }
-  }
-
-  return {
-    statusCode: 403,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-      'Access-Control-Allow-Headers': 'Content-Type',
-    },
-    body: JSON.stringify('Unauthorized website access. 😜'),
-  };
-};
-
-
-
-//  const authorizedDomains = require('./domains');
+// const fs = require('fs');
+// const path = require('path');
+// const authorizedDomains = require('./domains');
 
 // exports.handler = async (event, context) => {
 //   const { headers } = event;
 
 //   if ('origin' in headers) {
 //     const origin = headers['origin'];
-    
-//     if (authorizedDomains.includes(origin)) {
-//       const jsCode = `
-//         console.log("Successfully Connected With Server");
-//         fetch(feedUrl)
-//           .then(response => response.json())
-//           .then(data => {
-//             links.push(...(data.feed.entry || []).map(entry => entry.link.find(link => link.rel === "alternate").href));
-//           })
-//           .catch(error => console.error("Error fetching Posts:", error));
-//       `;
 
-//       return {
-//         statusCode: 200,
-//         headers: {
-//           'Access-Control-Allow-Origin': origin,
-//           'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-//           'Access-Control-Allow-Headers': 'Content-Type',
-//         },
-//         body: jsCode,
-//       };
+//     // Check if the requesting domain is authorized
+//     if (authorizedDomains.includes(origin)) {
+//       // Construct the file path
+//       const jsFilePath = path.resolve(__dirname, 'src/V3-latest.js');
+
+//       // Check if the file exists
+//       if (fs.existsSync(jsFilePath)) {
+//         console.log('File exists:', jsFilePath);
+
+//         // Read the content of the JavaScript file
+//         try {
+//           const jsCode = fs.readFileSync(jsFilePath, 'utf8');
+
+//           console.log('File read successfully');
+
+//           return {
+//             statusCode: 200,
+//             headers: {
+//               'Access-Control-Allow-Origin': origin,
+//               'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+//               'Access-Control-Allow-Headers': 'Content-Type',
+//             },
+//             body: jsCode,
+//           };
+//         } catch (error) {
+//           console.error('Error reading file:', error);
+
+//           return {
+//             statusCode: 500,
+//             headers: {
+//               'Access-Control-Allow-Origin': origin,
+//               'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+//               'Access-Control-Allow-Headers': 'Content-Type',
+//             },
+//             body: JSON.stringify({
+//               error: 'Internal Server Error',
+//               details: error.message,
+//             }),
+//           };
+//         }
+//       } else {
+//         console.error('File does not exist:', jsFilePath);
+
+//         return {
+//           statusCode: 404,
+//           headers: {
+//             'Access-Control-Allow-Origin': origin,
+//             'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+//             'Access-Control-Allow-Headers': 'Content-Type',
+//           },
+//           body: JSON.stringify({
+//             error: 'File Not Found',
+//           }),
+//         };
+//       }
 //     }
 //   }
 
 //   return {
 //     statusCode: 403,
-//     body: JSON.stringify('Code Is Not Found Here.. 😜'),
+//     headers: {
+//       'Access-Control-Allow-Origin': '*',
+//       'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+//       'Access-Control-Allow-Headers': 'Content-Type',
+//     },
+//     body: JSON.stringify('Unauthorized website access. 😜'),
 //   };
 // };
+
+
+
+ const authorizedDomains = require('./domains');
+
+exports.handler = async (event, context) => {
+  const { headers } = event;
+
+  if ('origin' in headers) {
+    const origin = headers['origin'];
+    
+    if (authorizedDomains.includes(origin)) {
+      const jsCode = `
+        console.log("Successfully Connected With Server");
+        fetch(feedUrl)
+          .then(response => response.json())
+          .then(data => {
+            links.push(...(data.feed.entry || []).map(entry => entry.link.find(link => link.rel === "alternate").href));
+          })
+          .catch(error => console.error("Error fetching Posts:", error));
+      `;
+
+      return {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': origin,
+          'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+          'Access-Control-Allow-Headers': 'Content-Type',
+        },
+        body: jsCode,
+      };
+    }
+  }
+
+  return {
+    statusCode: 403,
+    body: JSON.stringify('Code Is Not Found Here.. 😜'),
+  };
+};
